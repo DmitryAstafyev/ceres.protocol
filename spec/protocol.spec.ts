0ed924d4750602c5a9e19243e21ea623c4712a51
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-02-12 01:20:02 +0100

    [fix] fix ref to jasmine type definitions

--- spec/protocol.spec.ts
+++ spec/protocol.spec.ts
@@ -1,7 +1,7 @@
 // tslint:disable
 
-/// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
 
 //./node_modules/.bin/jasmine-ts src/something.spec.ts
 
